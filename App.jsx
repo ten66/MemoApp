93a2c3b391fd14f42c1ad4b3154aa2bbf9ddef68
ten66 <ikesho.1217@gmail.com>
2022-03-16 20:31:01 +0900

    #4 MemoListコンポーネントを作成

--- App.jsx
+++ App.jsx
@@ -2,43 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import AppBar from './src/components/AppBar';
+import MemoList from './src/components/MemoList';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <AppBar />
 
-      <View>
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2022年3月12日 10:00</Text>
-          </View>
-          <View>
-            <Text>x</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2022年3月12日 10:00</Text>
-          </View>
-          <View>
-            <Text>x</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2022年3月12日 10:00</Text>
-          </View>
-          <View>
-            <Text>x</Text>
-          </View>
-        </View>
-      </View>
+      <MemoList />
 
       <View style={styles.circleButton}>
         <Text style={styles.circleButtonLabel}>+</Text>
@@ -51,25 +22,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f0f4f8',
-  },
-  memoListItem: {
-    backgroundColor: '#ffffff',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingVertical: 16,
-    paddingHorizontal: 19,
-    alignItems: 'center',
-    borderBottomWidth: 1,
-    borderColor: 'rgba(0, 0, 0, 0.15)',
-  },
-  memoListItemTitle: {
-    fontSize: 16,
-    lineHeight: 32,
-  },
-  memoListItemDate: {
-    fontSize: 12,
-    lineHeight: 16,
-    color: '#848484',
   },
   circleButton: {
     backgroundColor: '#467fd3',
