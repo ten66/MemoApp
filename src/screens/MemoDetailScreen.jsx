e49e13d9512dd8e928b304512d6a48c25ae19b71
ten66 <ikesho.1217@gmail.com>
2022-03-17 10:39:14 +0900

    #8 vector-iconsパッケージをインストールし、プラスボタンを表示

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -24,7 +24,10 @@ export default function MemoDetailScreen() {
           本文用なので使い方を間違えると不自然に見えることもありますので要注意。
         </Text>
       </ScrollView>
-      <CircleButton style={{ top: 160, bottom: 'auto' }}>+</CircleButton>
+      <CircleButton
+        style={{ top: 160, bottom: 'auto' }}
+        name="plus"
+      />
     </View>
   );
 }
