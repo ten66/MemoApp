72b6c942ec2601723188d07883aee866c9377c80
ten66 <ikesho.1217@gmail.com>
2022-03-23 22:05:01 +0900

    #15 ログインページの作成

--- App.jsx
+++ App.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 // import MemoEditScreen from './src/screens/MemoEditSecreen';
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import MemoCreateScreen from './src/screens/MemoCreateScreen';
+import LogInScreen from './src/screens/LogInScreen';
 
 export default function App() {
   return (
-    <MemoCreateScreen />
+    <LogInScreen />
   );
 }
