98d70dab5ac3701a67bd73bc5f033ef5821e6bc2
ten66 <ikesho.1217@gmail.com>
2022-03-23 20:16:02 +0900

    #10 メモ編集ページのスタイリング

--- App.jsx
+++ App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditSecreen';
 
 export default function App() {
   return (
-    <MemoListScreen />
-    // <MemoDetailScreen />
+    <MemoEditScreen />
   );
 }
