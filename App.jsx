fef709af777ec1389b8d4d4742adb7639b879c90
ten66 <ikesho.1217@gmail.com>
2022-03-27 16:44:09 +0900

    #25 useStateの利用とtextInputのオプション追加

--- App.jsx
+++ App.jsx
@@ -15,7 +15,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="SignUp"
+        initialRouteName="LogIn"
         screenOptions={{
           headerStyle: { backgroundColor: '#467fd3' },
           headerTitle: 'Memo App',
